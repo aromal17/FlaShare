7bea8e8c4aeb1998e0d89c0dfa10b681a06c2612
aromal17 <aromalbenny17.com>
2021-08-12 19:57:24 +0530

    completed file upload to server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.post('/image', upload.single('image'), (req, res) => {
+    const file = req.file;
+    console.log("uploaded succesfully");
+    console.log(req.file);
+    res.send("File uploaded to server successfully");
+})
 
 app.listen(PORT, () => {
     console.log(`listening here at port ${PORT}`);
