f953d532c33a6bb8a6d7ac3726a377494cee7e93
aromal17 <aromalbenny17.com>
2021-08-13 13:39:06 +0530

    file upload function completed

--- s3.js
+++ s3.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const S3 = require("aws-sdk/clients/s3");
+const fs = require("fs");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
@@ -12,3 +13,17 @@ const s3 = new S3({
     secretAccessKey
 })
 
+//file upload to S3
+function uploadFile(file){
+    const fileStream = fs.createReadStream(file.path);
+
+    const uploadParams = {
+        Bucket : bucketName,
+        Body : fileStream,
+        key : file.filename //name or unique key with which the file is to be stored 
+    }
+
+    return s3.upload(uploadParams).promise();
+}
+
+exports.uploadFile = uploadFile;
